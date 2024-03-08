31c0dc6f37556790f2f8b27d40265c8aea7937d8
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-03-08 15:37:37 +0300

    Variables-and-types

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'Sura Itana';
+  int age = 22;
+  String favoriteColor = 'blue';
+
+  print('My name is $name.');
+  print('I am $age years old.');
+  print('My favorite color is $favoriteColor.');
+}
